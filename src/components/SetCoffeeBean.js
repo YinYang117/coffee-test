e8fe19e7781e0cf928f59d332eaffa6cee3f9ec5
andrew bui <bandrewi98@gmail.com>
2022-03-24 15:04:30 -0700

    phase 2 done

--- src/components/SetCoffeeBean.js
+++ src/components/SetCoffeeBean.js
@@ -1,9 +1,22 @@
+import { CoffeeContext } from "../context/CoffeeContext";
+import { useContext } from "react";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
+  const { coffeeBean, setCoffeeBeanId } = useContext(CoffeeContext)
+
+  // useEffect(() => {
+  //   setCoffeeBeanId()
+  // }, [coffeeBean])
+
   return (
     <div className="set-coffee-bean">
       <h2>Select a Coffee Bean</h2>
       <select
         name="coffee-bean"
+        value={coffeeBean.id}
+        onChange={(e) => {
+          setCoffeeBeanId(e.target.value)
+        }}
       >
         {coffeeBeans.map(bean => (
           <option
